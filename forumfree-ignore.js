42a0f8f8f1ff76b6cd8974111e2b6009f69959a6
ut0pic <43704493+ut0pic@users.noreply.github.com>
2018-09-30 17:32:06 +0200

    Update forumfree-ignore.js

--- forumfree-ignore.js
+++ forumfree-ignore.js
@@ -1,54 +1,111 @@
-/* TODO
- * 
- * ADD Ignore last post from ignored users in board view
- * ADD Ignore likes from ignored users
- */
-
-// execute the script before page load
-ignoreUsers("https://testf0rum.forumfree.it/?act=Msg&CODE=02", "TABLE", "quote_top");
-
 /*
- */
-async function ignoreUsers(url, postTag, quoteClass) {
-    /* --- get the ignore list --- */
-    const result = await fetch(url);
+*	TODO:
+*	- ignore user from topic list in sections/homepage
+*	- ignore likes from ignored users
+*/
+
+ignoreUsers();
+
+/* ----------------------------------------------------------- */
+/* --- get ignore list from logged user's forumfree        --- */
+/* --- contact list located at:                            --- */
+/* --- https://[forumName].forumfree.it/?act=Msg&CODE=02   --- */
+/* --- and remove mentions of ignored users from the page  --- */                         
+/* ----------------------------------------------------------- */
+
+async function ignoreUsers() {
+    
+    /* ----------------------------------------------------------- */
+    /* --- fetch the ignore list                               --- */
+    /* ----------------------------------------------------------- */
+    
+    const result = await /*fetch("https://"+document.domain+"/?act=Msg&CODE=02");*/ fetch("list.html");
     // set timeout otherwise text() fails/hangs
     setTimeout(() => null, 0);
     const data = await result.text();
     // replace '\n' with ' ' (space) in html document, otherwise regex does not match
     var s = data.replace(/\n/g, " ");
-    // regex to get the ignored
-    usersToIgnoreString = s.match(/<textarea name=\"cannot_contact\" .*?>(.*?)<\/textarea>/)[1];
+    // regex to get the user list
+    usersToIgnoreString = s.match
+    (/<textarea name=\"cannot_contact\" .*?>(.*?)<\/textarea>/)[1];
     // split the string at " " and put elements into an array
-    users = usersToIgnoreString.split(" ");
+    var users = usersToIgnoreString.split(" ");
     // last element is always empty, remove it
     users.splice(users.length-1, 1);
-    /* --- end --- */
     
-    /* --- ignore users from the list --- */
-    // iterate for every user in the list
+    /* ----------------------------------------------------------- */    
+
+    
+    /* ----------------------------------------------------------- */
+    /* --- ignore function                                     --- */
+    /* ----------------------------------------------------------- */
+  
     for(i = 0; i < users.length; i++){
-        var key = users[i];
-        console.log("key = "+users[i]);
-        /* --- remove user's posts from the page --- */
-        var posts = document.getElementsByTagName(postTag);
-        for(k = 0; k < posts.length; k++) {
-            if(posts[k].innerHTML.includes(key+"</a>") )
-                posts[k].style.display = "none";
+        var user = users[i];
+        if(window.location.href.includes("/?t=") ){ // topic ignore
+            // ignore user posts
+            ignorePosts(user);
+            // ignore user quotes
+            ignoreQuotes(user);
+        } else {
+        	if(window.location.href.includes("/?f=") ) // section/homepage ignore
+            	page = "SECTION";
+        	else
+            	page = "HOME";
+        	// ignore user mentions from topic list/homepage
+        	ignoreFromTopicList(user, page);
         }
-        /* --- end --- */
-        /* --- remove user's quoted posts from the page --- */
-        var quoteDivs = document.getElementsByClassName(quoteClass);
-        for(k = 0; k < quoteDivs.length; k++){
-            if(quoteDivs[k].innerHTML.includes(key+" @") ){
-                // get parent element
-                quotePost = quoteDivs[k].parentNode;
-                console.log("*****PARENT*****"+quotePost.innerHTML);
-                // hide quoted post
-                quotePost.style.display = "none";
-            }
-        }
-        /* --- end --- */
     }
-    /* --- end --- */
+  
+    /* ----------------------------------------------------------- */    
+}
+
+/* --- remove user's posts from the page --- */
+function ignorePosts(key){
+    var posts = document.getElementsByTagName("TABLE");
+    for(k = 0; k < posts.length; k++) {
+        if(posts[k].innerHTML.includes(key+"</a>") )
+            posts[k].style.display = "none";
+    }
+}
+
+/* --- remove quotes to ignored user from the page --- */
+function ignoreQuotes(key){
+    var quoteDivs = document.getElementsByClassName("quote_top");
+    for(k = 0; k < quoteDivs.length; k++){
+        if(quoteDivs[k].innerHTML.includes(key+" @") ){
+            // get parent element
+            quotePost = quoteDivs[k].parentNode;
+            // hide quoted post
+            quotePost.innerHTML ="<div align=\"center\"><div class=\"quote_top\" align=\"left\"><b>CITAZIONE</b> da utente ignorato</b></div></div>";
+        }
+    }
+}
+
+/* --- remove user's links from section/home views --- */
+function ignoreFromTopicList(key, view){
+    
+    if(view.includes("SECTION") )
+        tag = "when";
+    else if(view.includes("HOME") )
+        tag = "where";
+  
+    // get users mentions in the topic list
+	var lastPosts = document.getElementsByClassName("who");
+  	for(k = 0; k < lastPosts.length; k++){
+    	if(lastPosts[k].innerHTML.includes(key+"</a>") ){
+           // remove user mention
+           lastPosts[k].innerHTML = "<span>di</span> <a href=\""+window.location.href+"\">utente ignorato</a>";
+           // remove topic's last post reference
+           var node = document.getElementsByClassName(tag)[k];
+           document.getElementsByClassName(tag)[k].innerHTML = node.innerHTML.replace("#newpost","");
+    	}
+    }
+    // remove mentions from topics OP column
+    var topicOPs = document.getElementsByClassName("xx");
+    for(k = 0; k < topicOPs.length; k++){
+        if(topicOPs[k].innerHTML.includes(key+"</a>") )
+            // remove op mention
+            topicOPs[k].innerHTML = "<a href=\""+window.location.href+"\">utente ignorato</a>";
+    }
 }
